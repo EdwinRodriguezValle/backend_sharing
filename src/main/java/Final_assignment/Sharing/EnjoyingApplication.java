552f51a41b4b65cc7ef921c5ad15db365ad97600
EdwinRodriguezValle <rodriguez.valle.edwin@gmail.com>
2022-12-26 18:10:09 +0100

    first steps

---
+++ src/main/java/Final_assignment/Sharing/EnjoyingApplication.java
@@ -0,0 +1,13 @@
+package Final_assignment.Sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyingApplication.class, args);
+	}
+
+}
